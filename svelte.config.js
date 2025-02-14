03ea9a4e59e114a2d0941fbaf31ecc26fc945e84
lasse.knodt <lasse.knodt@student.fh-kiel.de>
2025-02-14 17:47:20 +0100

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,26 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const base = '/Portfolio'; // Repo-Name als Base-Pfad für GitHub Pages
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            pages: 'build', // Setzt das Build-Verzeichnis auf /build
+            assets: 'build', // Setzt das Assets-Verzeichnis auf /build
+            fallback: '404.html' // Notwendig für SPA-Routing
+        }),
         paths: {
-            base: '' // Entferne den Base-Pfad, da Vercel dies nicht benötigt
+            base: process.env.NODE_ENV === 'production' ? base : ''
         },
-        appDir: 'app'
+        appDir: 'app',
+		prerender: {
+			entries: ['*'] // Stellt sicher, dass alle statischen Dateien erfasst werden
+		}
+		
     }
 };
 
