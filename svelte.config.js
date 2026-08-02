1ba0108f1946ff59d4bbdf374f2944a79e8ac0b8
L4223 <lasse.knodt@online.de>
2026-08-02 20:38:43 +0200

    build ordner

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(), // Keine weiteren Optionen nötig für Vercel
+    adapter: adapter({
+      fallback: 'index.html'
+    })
   }
 };
 
